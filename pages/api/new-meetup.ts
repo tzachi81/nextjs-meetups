951ac6dead708a04f658c4ade08a31b75e917346
tzachi81 <tzahih@gmail.com>
2025-02-16 16:39:08 +0200

    Creates API handler

---
+++ pages/api/new-meetup.ts
@@ -0,0 +1,10 @@
+// /api/new-meetup
+
+export const handler = (request: Request, response: Response) => {
+
+  if(request.method === 'POST'){
+    const data = request.body; // new meetup form details sent
+
+    // const {title, image, address, description} = data;
+  }
+}
